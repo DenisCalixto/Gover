dfb3a84815ef96c7877bedf7d2b7eafc9f0eb2ab
DenisCalixto <deniscalixto@gmail.com>
2019-11-02 13:59:10 -0700

    Image upload POC code

--- app/src/main/java/edu/wmdd/gover/LoginActivity.java
+++ app/src/main/java/edu/wmdd/gover/LoginActivity.java
@@ -2,17 +2,24 @@ package edu.wmdd.gover;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,17 +28,23 @@ import com.android.volley.ServerError;
 import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends Activity {
 
     String username;
+    Button btnGoi,btnChonHinh;
+    ImageView imageView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,6 +53,10 @@ public class LoginActivity extends Activity {
 
         final EditText txtUsername = findViewById(R.id.user_name);
         final EditText txtPassword = findViewById(R.id.password);
+
+        btnChonHinh = (Button) findViewById(R.id.button2);
+        btnGoi = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         Button loginWithTokenButton = (Button) findViewById(R.id.loginButton);
         loginWithTokenButton.setOnClickListener(new View.OnClickListener() {
@@ -50,8 +67,109 @@ public class LoginActivity extends Activity {
             }
         });
 
+        btnChonHinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),999);
+            }
+        });
+
+        btnGoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //UploadImage();
+                saveProfileAccount();
+            }
+        });
+
         txtUsername.setHint(getString(R.string.username_hint));
         txtPassword.setHint(getString(R.string.password_hint));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 999 && resultCode== RESULT_OK && data !=null){
+            Uri filepath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filepath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void saveProfileAccount() {
+        // loading or check internet connection or something...
+        // ... then
+        VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, getString(R.string.api_property_url), new Response.Listener<NetworkResponse>() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+                String resultResponse = new String(response.data);
+                Log.d("Volley", resultResponse);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                String errorMessage = "Unknown error";
+                if (networkResponse == null) {
+                    if (error.getClass().equals(TimeoutError.class)) {
+                        errorMessage = "Request timeout";
+                    } else if (error.getClass().equals(NoConnectionError.class)) {
+                        errorMessage = "Failed to connect server";
+                    }
+                } else {
+                    String result = new String(networkResponse.data);
+                    try {
+                        JSONObject response = new JSONObject(result);
+                        String status = response.getString("status");
+                        String message = response.getString("message");
+
+                        Log.e("Error Status", status);
+                        Log.e("Error Message", message);
+
+                        if (networkResponse.statusCode == 404) {
+                            errorMessage = "Resource not found";
+                        } else if (networkResponse.statusCode == 401) {
+                            errorMessage = message+" Please login again";
+                        } else if (networkResponse.statusCode == 400) {
+                            errorMessage = message+ " Check your inputs";
+                        } else if (networkResponse.statusCode == 500) {
+                            errorMessage = message+" Something is getting wrong";
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.i("Error", errorMessage);
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("address","Image Test");
+                return params;
+            }
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                // file name could found file base or direct access from real path
+                // for now just get bitmap data from ImageView
+                params.put("thumbnail", new DataPart("property_thumbnail.png", AppHelper.getFileDataFromDrawable(getBaseContext(), imageView.getDrawable()), "image/png"));
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(multipartRequest);
     }
 
     private void login(String username, String password) {
